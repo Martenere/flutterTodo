a8e1d82323f8169f944277098727b92d21ac1903
Martenere <78728730+Martenere@users.noreply.github.com>
2022-10-27 12:37:15 +0200

    fff

--- lib/providers/todos_provider.dart
+++ lib/providers/todos_provider.dart
@@ -3,8 +3,8 @@ import 'package:lab1/main.dart';
 
 class TodosProvider with ChangeNotifier {
   List<Todo> _todos = <Todo>[
-    Todo("This data is provided from provider", false),
-    Todo("addMySec", true)
+    // Todo("This data is provided from provider", false),
+    // Todo("addMySec", true)
   ];
 
   List<Todo> get todos => _todos;
